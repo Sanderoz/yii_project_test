062fc687fa2621d770ddb58fddf473b6cb5aa12e
sandero <elizarov.c@mail.ru>
2022-10-13 09:48:53 +0500

    file vendor

---
+++ app/vendor/yiisoft/yii2-debug/vendor/yiisoft/extensions.php
@@ -0,0 +1,15 @@
+<?php
+
+$vendorDir = dirname(__DIR__);
+
+return array (
+  'yiisoft/yii2-swiftmailer' => 
+  array (
+    'name' => 'yiisoft/yii2-swiftmailer',
+    'version' => 'dev-master',
+    'alias' => 
+    array (
+      '@yii/swiftmailer' => $vendorDir . '/yiisoft/yii2-swiftmailer/src',
+    ),
+  ),
+);
--- mysql.Dockerfile
+++ mysql.Dockerfile
@@ -1,5 +1,5 @@
 FROM mysql:5.7
 
-ADD ./backup_mysql.sql /
+#ADD ./backup_mysql.sql /
 
 EXPOSE 3306
